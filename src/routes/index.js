9ae7faeae87b2d8027f2b940865a10f0e69bbbf8
Elisabet Salazar <eliisalazar502@gmail.com>
2024-02-25 11:38:21 -0500

    Fix code style

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,12 @@ const productRoutes = require("./product.routes")
 const commentRoutes = require("./comment.routes")
 
 function routerApi(app) {
-    const router = Router()
+	const router = Router()
 
-    app.use('/api/v1', router)
-    router.use('/users', userRoutes)
-    router.use('/products', productRoutes)
-    router.use('/comments', commentRoutes)
+	app.use('/api/v1', router)
+	router.use('/users', userRoutes)
+	router.use('/products', productRoutes)
+	router.use('/comments', commentRoutes)
 }
 
 module.exports = routerApi
